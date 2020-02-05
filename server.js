af58b2ffa78ea619569aa6be4332737305effb1c
nikola <nikola4work@gmail.com>
2020-02-05 12:09:54 -0500

    create note based on unique id & delete notes based on uniq id

--- server.js
+++ server.js
@@ -27,3 +27,30 @@ app.get("/api/notes", function(req, res) {
 app.get("/", function(res, req) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// Create New Note a new note using fs and JSON
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    newNote.id = uuidv1();
+    console.log(newNote);
+    var noteJson = getNoteJSON();
+    noteJson.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, "db", "db.json"),
+        JSON.stringify(noteJson)
+    );
+    res.json(noteJson);
+});
+
+// Delete a note based on the id
+app.delete("/api/notes/:id", function(req, res) {
+    var jsonContent = getNoteJSON();
+    var updatedJSON = jsonContent.filter(function(data) {
+        return data.id != req.params.id;
+    });
+    fs.writeFileSync(
+        path.join(__dirname, "db", "db.json"),
+        JSON.stringify(updatedJSON)
+    );
+    res.json(updatedJSON);
+});

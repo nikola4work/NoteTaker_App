55835be4988a151a1506baf2bc0d9f9269ed4102
nikola <nikola4work@gmail.com>
2020-02-05 11:58:47 -0500

    set up localhost to 1010 PORT & Data parsing

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require("express");
-var path = require('path');
-var fs = require('fs');
-var uuidv1 = require('uuid/v1');
+var path = require("path");
+var fs = require("fs");
+var uuidv1 = require("uuid/v1");
+
+var app = express();
+var PORT = process.env.port || 1010;
+
+app.use(express.urlencoded.apply({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
